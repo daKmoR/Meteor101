51657003be96749e22c514944692042719979c92
Thomas Allmer <d4kmor@gmail.com>
2014-03-12 16:54:42 +0100

    [TASK] Initial Page

--- client/lib/Textarea.js
+++
@@ -1,9 +0,0 @@
-Textarea = {
-	reset: function(textarea) {
-		$(textarea).blur();
-		$(textarea).val(''); //reset textarea
-		Meteor.setTimeout(function() {
-			$(textarea).focus();
-		}, 10); // fanzy enough we have to wait a split second or the cursor and whitespaces will stay
-	}
-};
--- server/publications.js
+++
@@ -1,7 +0,0 @@
-Meteor.publish('items-public', function () {
-	return Items.find({public: true});
-});
-
-Meteor.publish('items-all', function () {
-	return Items.find({});
-});
